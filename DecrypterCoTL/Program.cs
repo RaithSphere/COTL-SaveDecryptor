c49cbd63a514325c9efc8e2457e754dad1795b2b
RaithSphere <RaithSphere@gmail.com>
2024-01-19 01:14:19 +0000

    Initial commit

---
+++ DecrypterCoTL/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+
+class Decrypter_CoTL
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Usage: DecryptFile.exe <input_file>");
+            return;
+        }
+
+        string filePath = args[0];
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine($"File not found: {filePath}");
+            return;
+        }
+
+        byte[] encryptedBytes = File.ReadAllBytes(filePath);
+        byte[] keyBytes = encryptedBytes.Skip(1).Take(16).ToArray();
+        byte[] ivBytes = encryptedBytes.Skip(17).Take(16).ToArray();
+        byte[] dataBytes = encryptedBytes.Skip(33).ToArray();
+
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = keyBytes;
+            aesAlg.IV = ivBytes;
+
+            using (MemoryStream msDecrypt = new MemoryStream(dataBytes))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, aesAlg.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        string decryptedText = srDecrypt.ReadToEnd();
+                        string beautifiedJson = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(decryptedText), Formatting.Indented);
+                        Console.WriteLine(beautifiedJson);
+                        string originalFileNameWithoutExtension = Path.GetFileNameWithoutExtension(filePath);
+                        string newFileName = $"{originalFileNameWithoutExtension}_decrypted.json";
+                        File.WriteAllText(newFileName, beautifiedJson);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+<h1 align="center">Cult of the Lamb Save Decryptor/h1>
+
+
+## Usage
+
+To use the Cult of the Lamb Save Decryptor follow these steps:
+
+1. Open a command prompt or terminal.
+2. Navigate to the directory containing the `Decrypter.exe` file.
+3. Run the program with the following command: Decrypter.exe <input_file> or drag and drop the file on it
+
+this will create a new file called slot_x_decrypted.json which you can then edit in your favorite editor and then save, CoTL will be able to read this file and will then encrypt it when you save!
+
+Save file location Windows: C:\Users\%USERPROFILE%\AppData\LocalLow\Massive Monster\Cult Of The Lamb\saves
